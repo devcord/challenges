6122c1d597c84b5ff0d75a09b2cd399e25b54893
Andr3wRiv3rs <andrewarivers@gmail.com>
2019-04-29 18:56:38 -0700

    Discord Module Separation/Axios Integration

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const fs = require('fs')
-const btoa = require('btoa')
-const fetch = require('node-fetch');
 const crypt = require('./crypt')
+const discord = require('./discord')
 
 const app = express()
 
@@ -19,6 +18,12 @@ const errors = {
         message: 'internal server error'
     }
 }
+
+discord.init({
+    id: process.env.CLIENT_ID,
+    secret: process.env.CLIENT_SECRET,
+    token: process.env.BOT_TOKEN
+})
 
 const db = new(require('nosqlite').Connection)('./data')
 const challenge = db.database('challenge')
@@ -40,63 +45,22 @@ app.get('/test', (req,res) => {
 
 app.get('/login', (req, res) => {
     res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${
-        process.env.CLIENT_ID
+        discord.credentials.id
     }&scope=identify&response_type=code&redirect_uri=${
-        encodeURIComponent(getURL(req, 'api/discord/auth'))
-    }`);
-})
-
-const discord = async (path, Authorization, method) => await (
-    await fetch(`https://discordapp.com/api/${path}`, {
-        method, headers: { Authorization },
-    })
-).json()
-
-const getAvatar = data => data.avatar ? `https://cdn.discordapp.com/avatars/${
-    data.id
-}/${
-    data.avatar
-}.png` : `https://cdn.discordapp.com/embed/avatars/${
-    data.discriminator % 5
-}.png`
-
-const getCurrentUserData = async token => {
-    const data = await (await discord('users/@me', `Bearer ${ token }`))
-
-    return {
-        ...data,
-        avatar_url: getAvatar(data)
-    }
-}
-
-const getUserData = async id => {
-    const data = await (await discord(`users/${id}`, `Bot ${ process.env.BOT_TOKEN }`))
-
-    return {
-        ...data,
-        avatar_url: getAvatar(data)
-    }
-}
-
-app.get('/users/@me', async (req,res) => {
-    try {
-        const {access_token} = JSON.parse(req.cookies.discord_token) 
-        
-        res.json({
-            success: true,
-            user: await getCurrentUserData(access_token)
-        })
-    } catch (error) {
-        res.json(errors.internal)
-        console.log(error)
-    }
+        encodeURIComponent(getURL(req, 'api/discord/code'))
+    }`)
 })
 
 app.get('/users/:id', async (req,res) => {
     try {
+        const {id} = req.params
+        const {access_token} = JSON.parse(req.cookies.discord_token)
+
         res.json({
             success: true,
-            user: await getUserData(req.params.id)
+            user: id === '@me' 
+                ? await discord.user({ token: access_token })
+                : await discord.user({ id })
         })
     } catch (error) {
         res.json(errors.internal)
@@ -114,34 +78,30 @@ app.get('/discord/refresh', async (req,res) => {
 
 app.get('/discord/code', async (req,res) => {
     try {
-        let response = await discord(`oauth2/token?grant_type=authorization_code&code=${
-            req.query.code
-        }&redirect_uri=${
-            encodeURIComponent(getURL(req, 'api/discord/auth'))
-        }`,`Basic ${
-            btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`)
-        }`, 'POST');
+        const response = await discord.code({
+            code: req.query.code,
+            redirect_uri: encodeURIComponent(getURL(req, 'api/discord/code'))
+        })
 
-        const { access_token, expires_in, refresh_token } = response
-
+        const { access_token, refresh_token, expires_in } = response.token
+        const { user } = response
+    
         res.cookie('discord_token', JSON.stringify({
-            access_token,expires_in
+            access_token, expires_in
         }))
 
-        const user = await getCurrentUserData(access_token)
-
+        auth.postSync({
+            id: user.id,
+            refresh_token: crypt.encrypt(refresh_token, access_token.padEnd(32,'0'))
+        })
+    
         res.json({
             success: true,
             user
         })
-
-        auth.postSync({
-            id: user.id,
-            refresh_token: crypt.encrypt(refresh_token, access_token)
-        })
     } catch (error) {
-        res.json(errors.internal)
         console.log(error)
+        res.json(errors.internal)
     }
 })
 
